b7eba1e125612e9bd8b3eb3bf2cbf7e22fbf0e9b
mynkow <mynkow@gmail.com>
2022-03-02 16:17:58 +0200

    Adds connection serializer attributesa

--- src/Elders.Cronus.Dashboard/Models/Connection.cs
+++ src/Elders.Cronus.Dashboard/Models/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Elders.Cronus.Dashboard.Models
 {
@@ -20,12 +21,15 @@ namespace Elders.Cronus.Dashboard.Models
             this.oAuth = oAuth ?? new oAuth();
         }
 
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [JsonPropertyName("cronusEndpoint")]
         public string CronusEndpoint { get; set; }
 
         public oAuth oAuth { get; set; }
 
+        [JsonPropertyName("oAuths")]
         public List<oAuth> oAuths { get; set; }
     }
 
@@ -42,16 +46,22 @@ namespace Elders.Cronus.Dashboard.Models
             Tenant = tenant;
         }
 
+        [JsonPropertyName("serverEndpoint")]
         public string ServerEndpoint { get; set; }
 
+        [JsonPropertyName("client")]
         public string Client { get; set; }
 
+        [JsonPropertyName("secret")]
         public string Secret { get; set; }
 
+        [JsonPropertyName("scope")]
         public string Scope { get; set; }
 
+        [JsonPropertyName("basicAuthorization")]
         public string BasicAuthorization => Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Client}:{Secret}"));
 
+        [JsonPropertyName("tenant")]
         public string Tenant { get; set; }
     }
 }
